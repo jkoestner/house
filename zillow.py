00edb615295784b4be4ac9b85a4f08e747558523
John Koestner <johnkoestner@outlook.com>
2020-09-28 23:16:49 -0400

    updated headers

--- zillow.py
+++ zillow.py
@@ -60,12 +60,11 @@ def parse(zipcode, filter=None, fav=None):
     while url_error < 1:
 
         headers = {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "accept-encoding": "gzip, deflate, sdch, br",
-            "accept-language": "en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4",
-            "cache-control": "max-age=0",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-US,en;q=0.8",
             "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
         }
         url_page = (
             "https://www.zillow.com/homes/for_sale/2_p/?searchQueryState=%7B%22"
@@ -139,6 +138,10 @@ def parse(zipcode, filter=None, fav=None):
         )
         try:
             response = requests.get(url_page, headers=headers)
+            parser = html.fromstring(response.text)
+            search_results = parser.xpath("//div[@id='grid-search-results']//article")
+            if search_results == []:
+                url_error = 1
         except:
             url_error = 1
             pass
